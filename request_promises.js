5fba0f248a9fa4e37d4a52676c524f80ef7333b5
jackeraf <jarekimar@yahoo.es>
2017-04-23 00:56:51 +0200

    getting response with promise.all

--- request_promises.js
+++ request_promises.js
@@ -4,22 +4,60 @@ var request = require('request-promise');
 function findMovie(title){
 	return request(`http://www.omdbapi.com/?t=${title}`)
 	//request devuelve una promise por eso then:
-		.then(res=>{
-			var movie= JSON.parse(res)
-			return movie
-		})
+	.then(res=>{
+		var movie= JSON.parse(res)
+		return movie
+	})
 }
 
-function loadInitialData(){
-	var promise= findMovie("The Matrix")
-		promise.then((movie)=>{
-			console.log("inside loadInitialData")
-			console.log(movie["Title"])
-			console.log(movie["Year"])
-			console.log(movie["Genre"])
-		})
+function loadInitialData(movies){
+	//console.log(movies[0])
+	//findMovie()
+
+			Promise.all(movies)
+			.then((response)=>{
+				console.log(response)
+				console.log(response["Title"])
+				console.log(response["Year"])
+				console.log(response["Genre"])
+			})
+	
+	
 }
 
-loadInitialData()
 
 
+
+var movies= [findMovie("jason bourne"), findMovie("The Matrix"), findMovie("titanic")]
+
+loadInitialData(movies)
+
+
+/*
+var p1= new Promise((resolve, reject)=>{
+setTimeout(()=>{
+resolve("One")
+},3000)
+})
+
+var p2= new Promise((resolve, reject)=>{
+setTimeout(()=>{
+resolve("Two")
+},1000)
+})
+
+var p3= new Promise((resolve, reject)=>{
+setTimeout(()=>{
+resolve("Three")
+},4000)
+})
+
+Promise.all([p1, p2, p3])
+.then((values)=>{
+console.log(values)
+})
+.catch((err)=>{
+console.log(err)
+})
+
+*/
