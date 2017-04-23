80a4298de0e13cebffe45542e9ef891cd1cdb1b8
jackeraf <jarekimar@yahoo.es>
2017-04-23 21:29:32 +0200

    whileDo working OK

--- request_promises.js
+++ request_promises.js
@@ -1,37 +1,51 @@
 var request = require('request-promise');
 
 
-function findMovie(title){
-	return request(`http://www.omdbapi.com/?t=${title}`)
-	//request returns a promise, therefore then:
-	.then(res=>{
-		var movie= JSON.parse(res)
-		return [movie["Title"], movie["Year"],movie["Genre"]]
-	})
-}
+const results = [];
 
-function loadInitialData(movies){
+function whileDo(condition, job) {
 
-			return Promise.all(movies)
-			.then((response)=>{
-				
-				response.forEach((movie)=>{
-					console.log(movie[1])
+	return new Promise((resolve, reject) => {
+let counter = 0;
+
+		function next() {
+			counter += 1;
+			if (!condition(counter - 1)) return resolve();
+			return job(counter - 1).then(next, reject);
+		};
+
+		next();
+
+	});
+};
+
+
+function findMovie(movies){
+	
+		return whileDo(
+			(i) =>  i < movies.length,
+			(i) => {
+		  
+		  	
+		  	return request(`http://www.omdbapi.com/?t=${movies[i]}`)
+		  		.then(res=>{
+
+					var movie= JSON.parse(res)
+					console.log(movie["Title"], movie["Year"],movie["Genre"])
+					return [movie["Title"], movie["Year"],movie["Genre"]]
 				})
-			})
+		  
+		}
+		);
 	
 	
 }
 
 
 
+var movies= ["jason bourne", "The Matrix", "titanic"]
 
-//var movies= [findMovie("jason bourne"), findMovie("The Matrix"), findMovie("titanic")]
-var movies= ["jason bourne", "The Matrix", "titanic"].map(function (movie) {
-  return findMovie(movie);
-});
-
-loadInitialData(movies)
+findMovie(movies)
 
 
 
